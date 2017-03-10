6bb78cbdaab1a667dea0b65a951c93ad181b091c
Rebecca Leonard <rebeccadleonard@gmail.com>
2017-03-10 02:23:30 +0000

    previewing the email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-
+  default from: "no-reply@nomster.com"
   def comment_added
     mail(to: "rebeccadleonard@gmail.com", subject: "A comment has been added to your place")
   end
