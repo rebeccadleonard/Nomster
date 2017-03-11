026c9a158661599aa79fcfaa511b84a90f6d1b24
Rebecca Leonard <rebeccadleonard@gmail.com>
2017-03-11 20:37:12 +0000

    send email on comment 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.delivery_method = :test
+=begin
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -16,7 +16,7 @@ Rails.application.configure do
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
-
+=end
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
 
   config.cache_classes = false
